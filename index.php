2cbcdb0bd09cbe6b21f911dd6dd7ff736cd00015
chri20h1 <58981824+chri20h1@users.noreply.github.com>
2020-08-19 17:39:39 +0200

    commit test 4

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
        <section class="section-default">
          <?php
           if (isset($_SESSION['userId'])) {
-            echo '<p class="login-status">Du er logget på!</p>';
+            echo '<p class="login-status">Du er logget på!!!</p>';
           }
           else {
             echo '<p class="login-status">Du er ikke logget på!</p>';
