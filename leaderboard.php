1c4bbba0babd504e3b80f7ced9545b115f477e07
dani447eTietgen <dani447e@tietgen.dk>
2021-06-03 15:31:05 +0200

    lead

--- leaderboard.php
+++ leaderboard.php
@@ -1,19 +1,20 @@
 <?php
   include_once 'header.php';
 ?>
-
+  <link rel="stylesheet" href="styles/grid.css"/>
    <main>
      <div class="wrapper-main">
        <section class="section-default">
          <?php if (isset($_SESSION['useruid'])) : ?>
 
-            <p class="login-status">Du er logget på!!!!</p>
+            <p class="login-status">Tietgens officielle leaderboard</p>
 
           <?php else : ?>
 
             <p class="login-status">Du er ikke logget på!</p>
 
            <?php endif; ?>
+
        </section>
       </div>
      </main>
