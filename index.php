f6dc38154d08ac6a40cdc9ac450d5ae2a7a13de3
dani447eTietgen <dani447e@tietgen.dk>
2021-06-03 14:21:31 +0200

    clean up på index

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
        <?php
         if (isset($_SESSION['useruid'])) {
-          echo '<section class="section-default"><p class="login-status">Du er logget på!!!!</p></section>';
+          echo '<section class="section-default"><p class="login-status">Tiegten Game Portal</p></section>';
        }
         else {
           echo '<img class="forside" src="img/tietPic.png" alt="logo" width="100%" height="100%">';
@@ -32,7 +32,10 @@ er en meget stor ære</p>
 <p><a href="games.php" class="button1">Snake</a></p>
          </div>
          <div class="footer">
-<h1>:)</h1>
+<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
          </div>
        </div>
 
